930f31031e0e95b666a899e808f9536dd54d1f2d
AnjanaS123 <96466216+AnjanaS123@users.noreply.github.com>
2022-06-03 13:30:06 +0530

    PL/SQL program for sum of n natural numbers

---
+++ sum_n_num.sql
@@ -0,0 +1,23 @@
+
+ DECLARE
+ sumVal NUMBER;
+ n NUMBER;
+ i NUMBER;
+ 
+ FUNCTION Findmax(n IN NUMBER)
+    RETURN NUMBER
+ IS
+    sums NUMBER := 0;
+ BEGIN
+    FOR i IN 1..n
+    LOOP
+    sums := sums + i*(i+1)/2;
+    END LOOP;
+    RETURN sums;
+ END;
+ BEGIN
+    n := 8;
+    sumVal := findmax(n);
+    dbms_output.Put_line('Sum of natural numbers is ' || sumVal);
+ END;
+ /
